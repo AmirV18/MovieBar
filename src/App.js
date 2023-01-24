ec402939a541e68c115d737e12343cc073ed9348
AmirV18 <amirvassell1@gmail.com>
2023-01-24 01:26:39 -0500

    Completed react movie app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,74 @@
-import React from "react";
+//import React from "react";
+import { useState, useEffect } from 'react';
 
-const App = () => {
+import MovieCard from "./MovieCard"
+
+import './App.css';
+import SearchIcon from './search.svg';
+
+//eafee3d2
+
+const API_URL = 'http://www.omdbapi.com?apikey=eafee3d2';
+
+const movie1 = {
+   "Title": "Amazing Spiderman Syndrome",
+   "Year": "2012",
+   "imdbID": "tt2586634",
+   "Type": "movie",
+   "Poster": "N/A"
+}
+
+const App = () => { //Main component that is rendered in index.js
+
+   const [movies, setMovies] = useState([]);
+   const [searchTerm, setSearchTerm] = useState('');
+
+   const searchMovies = async (title)=> {
+      const response = await fetch(`${API_URL}&s=${title}`);
+      const data = await response.json();
+
+      setMovies(data.Search);
+   }
+
+   useEffect(() =>{ //useEffect is a function that takes two parameters, a callback funciton and an array
+      searchMovies('Spiderman');
+
+   }, []);
+
    return(
-      <h1>App</h1>
+      <div className='app'>
+         <h1>MovieLand</h1>
+
+         <div className='search'>
+            <input
+               placeholder='Search for movies'
+               value={searchTerm}
+               onChange={(e) => setSearchTerm(e.target.value)}
+            />
+            <img
+               src = {SearchIcon}
+               alt = "search"
+               onClick={() => searchMovies(searchTerm)}
+            />
+         </div>
+
+         {
+            movies?.length > 0
+            ?(
+            <div className='container'>
+               {movies.map((movie) => (
+                  <MovieCard movie = {movie} /> //passing it as a prop
+               ))}
+            </div>
+            ) :
+            (
+               <div className='empty'>
+                  <h2>No movies found</h2>
+               </div>   
+            )
+         }
+
+      </div>
    );
 }
 
--- src/index.js
+++ src/index.js
@@ -4,3 +4,4 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+//root is rendered from the index.html in public. It's a div that is rendered with what's in app.js
